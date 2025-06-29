760eb4cbc708408c08b0280b0a7e1f66df8aa8cc
Bhumika kashyap <tilantajagra@gmail.com>
2025-06-29 20:59:33 +0530

    Update environment.js

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,7 @@
 let IS_PROD = true;
 
 const server = IS_PROD
-  ? "https://echomeet-server.onrender.com" // <-- Replace with your deployed backend URL
+  ? "https://echomeet-backend0.onrender.com" // <-- Replace with your deployed backend URL
   : "http://localhost:8000";
 
 export default server;
